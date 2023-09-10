340ab0f7f844262ab5881ab227d59039199cdf35
Anuj Singh <anujk920@gmail.com>
2023-09-10 22:30:02 +0530

    Added Example Executor For Better Run

---
+++ superpilot/examples/executor/__init__.py
@@ -0,0 +1,13 @@
+from superpilot.examples.executor.midjourney_prompt import (
+    MidjourneyPromptPromptExecutor,
+)
+from superpilot.examples.executor.stabledifusion_prompt import (
+    StableDiffusionPromptExecutor,
+)
+from superpilot.examples.executor.stable_diffusion import StableDiffusionImageExecutor
+
+__all__ = [
+    "MidjourneyPromptPromptExecutor",
+    "StableDiffusionPromptExecutor",
+    "StableDiffusionImageExecutor",
+]
