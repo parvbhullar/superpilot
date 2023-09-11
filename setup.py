dc7fe142d1fd051be46b0de17018b9023e5f7997
Anuj Singh <anujk920@gmail.com>
2023-09-12 02:05:08 +0530

    rename

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
 
 setup(
     name="superpilot",
-    version="0.2",
+    version="0.3",
     description="The Multi-Role Superpilot Programming Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
