b72ae02e83fca8539530359698c3914766eea9d1
Anuj Singh <anujk920@gmail.com>
2023-09-16 16:03:00 +0530

    Add Question Identifier Example

--- superpilot/examples/executor/__init__.py
+++ superpilot/examples/executor/__init__.py
@@ -5,9 +5,13 @@ from superpilot.examples.executor.stabledifusion_prompt import (
     StableDiffusionPromptExecutor,
 )
 from superpilot.examples.executor.stable_diffusion import StableDiffusionImageExecutor
+from superpilot.examples.executor.question_identifier import (
+    QuestionIdentifierPromptExecutor,
+)
 
 __all__ = [
     "MidjourneyPromptPromptExecutor",
     "StableDiffusionPromptExecutor",
     "StableDiffusionImageExecutor",
+    "QuestionIdentifierPromptExecutor",
 ]
