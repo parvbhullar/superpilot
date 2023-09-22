11f06e692ca478b6489f0a05f7df9f494328e4e2
Anuj Singh <anujk920@gmail.com>
2023-09-23 01:24:47 +0530

    question identifier via List code Added.

--- superpilot/tests/test_executor.py
+++ superpilot/tests/test_executor.py
@@ -36,28 +36,43 @@ import pandas as pd
 #     print(item.description)
 #     Image.open(item.file_path).show()
 
-cd_prompt = ClipDropImageExecutor()
-from PIL import Image
+# cd_prompt = ClipDropImageExecutor()
+# from PIL import Image
 
-print("\n", "*" * 32, "Running ClipDropImageExecutor", "*" * 32, "\n\n")
-context = asyncio.run(cd_prompt.run("A photo of a cat"))
-for item in context.items:
-    print(item.description)
-    Image.open(item.file_path).show()
-
+# print("\n", "*" * 32, "Running ClipDropImageExecutor", "*" * 32, "\n\n")
+# context = asyncio.run(cd_prompt.run("A Man drinking beer & playing with dog"))
+# for item in context.items:
+#     print(item.description)
+#     Image.open(item.file_path).show()
 
 # data_df = pd.read_excel("/home/dora/Downloads/Cloudbird Done Data.xlsx", sheet_name="9 sep")
 content = """
 A landscape space that is dominated by white, grey, blue, and green colors, suggest which of the following sensory responses of human actions?  A) Gay, lively  B) Relaxing, stable  C) Frightening, inhibiting  D) Regressive, detached
 """
 
-content = """
-Which of the following is not true regarding data mining? _______ A) It focuses on discovering useful information.  B) It can use machine learning techniques. C) It is one task within a process. D) It is a process from start to end."""
+# content = """
+# Which of the following is not true regarding data mining? _______ A) It focuses on discovering useful information.  B) It can use machine learning techniques. C) It is one task within a process. D) It is a process from start to end."""
+# t1 = time.time()
+# sd_prompt = QuestionIdentifierPromptExecutor()
+# print("\n", "*" * 32, "Running QuestionIdentifierPromptExecutor", "*" * 32, "\n\n")
+# res = asyncio.run(sd_prompt.run(content))
+# print(res.content.get("content"))
+# print(res.content.get("status"))
+# t2 = time.time()
+# print("Time Taken", round(t2 - t1, 2), "seconds")
+
+
+data_df = pd.read_excel(
+    "/home/mastersindia/Documents/ML_Docs/Cloudbird Done Data.xlsx", sheet_name="9 sep"
+)
+smaple_data = data_df[:10].reindex(columns=["Original keyword"])
+print(smaple_data.shape)
 t1 = time.time()
 sd_prompt = QuestionIdentifierPromptExecutor()
 print("\n", "*" * 32, "Running QuestionIdentifierPromptExecutor", "*" * 32, "\n\n")
-res = asyncio.run(sd_prompt.run(content))
-print(res.content.get("content"))
-print(res.content.get("status"))
+
+res = asyncio.run(sd_prompt.run_list(smaple_data.to_dict("records")))
 t2 = time.time()
 print("Time Taken", round(t2 - t1, 2), "seconds")
+final_df = pd.DataFrame(res)
+final_df.to_excel("final_response.xlsx")
