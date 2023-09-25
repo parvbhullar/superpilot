bdea53c1caaa979243880ecef08a102a5ce04b85
Anuj Singh <anujk920@gmail.com>
2023-09-26 00:43:22 +0530

    Add run file Logic

--- superpilot/examples/executor/question_extractor.py
+++ superpilot/examples/executor/question_extractor.py
@@ -3,17 +3,17 @@ from typing import Dict, List
 from superpilot.core.configuration.config import get_config
 
 from superpilot.core.context.schema import Context
+
 # from superpilot.core.planning.schema import Task
 from superpilot.core.resource.model_providers.factory import ModelProviderFactory
 from superpilot.examples.abilities.question_extractor import QuestionExtractor
 from superpilot.examples.executor.base import BaseExecutor
 from superpilot.examples.executor.latex_code_generator import LatexCodeGenExecutor
+
 # from superpilot.examples.pilots.tasks.super import SuperTaskPilot
 from superpilot.tests.test_env_simple import get_env
 
-ALLOWED_ABILITY = {
-    QuestionExtractor.name(): QuestionExtractor.default_configuration
-}
+ALLOWED_ABILITY = {QuestionExtractor.name(): QuestionExtractor.default_configuration}
 
 
 class QuestionExtractorExecutor(BaseExecutor):
@@ -25,7 +25,7 @@ class QuestionExtractorExecutor(BaseExecutor):
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-        
+
         self.ability = QuestionExtractor(environment=self.env)
 
     async def run(self, query):
@@ -38,12 +38,19 @@ class QuestionExtractorExecutor(BaseExecutor):
         print("\n", "*" * 32, "Running LatexCodeGenExecutor", "*" * 32, "\n\n")
         content = await latex_convertor.run(str(context))
         return content
-    
+
     async def run_list(self, query_list: List[Dict]):
         final_res = []
-        for index, query in enumerate(query_list):
-            response = await self.run(query.get("Original Keyword"))
-            print("Response", response)
-            final_res.append({**query, **response.content, "total_cost($)": response.total_cost})
-            print(f"Query {index} finished", "\n\n")
-        return final_res
+        error_res = []
+        try:
+            for index, query in enumerate(query_list):
+                response = await self.run(query.get("Original Keyword"))
+                print("Response", response)
+                final_res.append(
+                    {**query, **response.content, "total_cost($)": response.total_cost}
+                )
+                print(f"Query {index} finished", "\n\n")
+        except Exception as e:
+            print(e)
+            error_res.append(query)
+        return final_res, error_res
---
+++ superpilot/tests/run_file.py
@@ -0,0 +1,63 @@
+from concurrent import futures
+import os
+import sys
+import asyncio
+import time
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from superpilot.examples.executor import QuestionExtractorExecutor
+
+import pandas as pd
+
+run_timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+
+def process_single_file(chunk, index):
+    t1 = time.time()
+    sd_prompt = QuestionExtractorExecutor()
+    print(
+        "\n", "*" * 32, "Running QuestionExtractorExecutor " + index, "*" * 32, "\n\n"
+    )
+    success, error = asyncio.run(sd_prompt.run_list(chunk))
+    t2 = time.time()
+    print("Time Taken", round(t2 - t1, 2), "seconds")
+    if len(success) > 0:
+        success_df = pd.DataFrame(success)
+        success_df.to_csv(
+            "latex_response_" + index + run_timestamp + ".csv", mode="a+", index=False
+        )
+    if len(error) > 0:
+        error_df = pd.DataFrame(error)
+        error_df.to_csv(
+            "latex_error_" + index + run_timestamp + ".csv", mode="a+", index=False
+        )
+
+
+def run_file_with_search():
+    file_location = ""
+    data_df = pd.read_csv(file_location)
+    data_df = data_df.reindex(columns=["Original Keyword"])
+    last_index = open("last_index.txt", "r").read()
+    if last_index == "":
+        last_index = 0
+    else:
+        last_index = int(last_index)
+
+    file_size = 100
+    max_workers = 10
+    total_data = len(data_df)
+    if file_size <= last_index:
+        print("No Processing Done", file_location)
+        return True
+
+    with futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for i in range(max_workers):
+            if file_size <= last_index:
+                break
+            chunk = data_df[last_index : last_index + file_size]
+            last_index += file_size
+            executor.submit(
+                process_single_file, chunk.to_dict(orient="records"), str(i)
+            )
+    with open("last_index.txt", "w") as f:
+        f.write(str(last_index))
