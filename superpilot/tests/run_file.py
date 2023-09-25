9d009db34dd4e8417d418b418d1327cef801867d
Anuj Singh <anujk920@gmail.com>
2023-09-26 01:02:30 +0530

    Some Fix in run

--- superpilot/tests/run_file.py
+++ superpilot/tests/run_file.py
@@ -23,27 +23,32 @@ def process_single_file(chunk, index):
     print("Time Taken", round(t2 - t1, 2), "seconds")
     if len(success) > 0:
         success_df = pd.DataFrame(success)
-        success_df.to_csv(
-            "latex_response_" + index + run_timestamp + ".csv", mode="a+", index=False
+        success_df.to_excel(
+            "latex_response_" + index + "_" + run_timestamp + ".csv",
+            mode="a+",
+            index=False,
         )
     if len(error) > 0:
         error_df = pd.DataFrame(error)
-        error_df.to_csv(
-            "latex_error_" + index + run_timestamp + ".csv", mode="a+", index=False
+        error_df.to_excel(
+            "latex_error_" + index + "_" + run_timestamp + ".csv",
+            mode="a+",
+            index=False,
         )
 
 
 def run_file_with_search():
-    file_location = ""
+    file_location = "/home/mastersindia/Documents/Personal/Knowledge/chat/superpilot/superpilot/docs/QuestionsData-Sheet3.csv"
     data_df = pd.read_csv(file_location)
     data_df = data_df.reindex(columns=["Original Keyword"])
+    os.path.exists("last_index.txt") or open("last_index.txt", "w+").write("")
     last_index = open("last_index.txt", "r+").read()
     if last_index == "":
         last_index = 0
     else:
         last_index = int(last_index)
 
-    file_size = 100
+    file_size = 10
     max_workers = 10
     total_data = len(data_df)
     if total_data <= last_index:
@@ -55,12 +60,17 @@ def run_file_with_search():
             if total_data <= last_index:
                 break
             chunk = data_df[last_index : last_index + file_size]
-            last_index += file_size
+            last_index += len(chunk)
             executor.submit(
                 process_single_file, chunk.to_dict(orient="records"), str(i)
             )
     with open("last_index.txt", "w+") as f:
         f.write(str(last_index))
+    return False
 
 
-run_file_with_search()
+while True:
+    res = run_file_with_search()
+    if res:
+        break
+    time.sleep(5)
