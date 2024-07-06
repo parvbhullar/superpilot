f68727d0172c31f42698655f477ab8c7d46a0663
Shivam <shivamgarg260@gmail.com>
2024-07-06 15:08:59 +0530

    add dynamic conf to dynamic ability run

--- superpilot/tests/test_block.py
+++ superpilot/tests/test_block.py
@@ -13,38 +13,6 @@ from superpilot.core.block.base import BlockConfiguration
 import asyncio
 
 json_data = [
-        # {
-        #     'id': 4,
-        #     'created': datetime.datetime(2024, 4, 30, 18, 38, 35, 596732),
-        #     'modified': datetime.datetime(2024, 4, 30, 18, 40, 19, 84799),
-        #     'created_by': None,
-        #     'updated_by': None,
-        #     'block_type': 'input_form',
-        #     'metadata': '{}',
-        #     'seq_order': 1,
-        #     'block_visibility': 1,
-        #     'result_visibility': 1,
-        #     'input_schema': '[{"name": "statement", "type": "text", "title": "sentence", "required": true, "placeholder": "sentence", "defaultValue": "Hello"}, {"name": "source_language", "type": "select", "title": "Source Language", "choices": ["Hindi", "English", "Punjabi"], "required": true, "placeholder": "Single select", "defaultValue": "Hindi"}, {"name": "translation_language", "type": "select", "title": "Translation Language", "choices": ["English", "Hindi", "Punjabi", "Tamil"], "required": true, "placeholder": "Single select", "defaultValue": "English"}]',
-        #     'output_schema': '[]',
-        #     'body': '',
-        #     'superbook_id': 21
-        # },
-        # {
-        #     'id': 6,
-        #     'created': datetime.datetime(2024, 5, 19, 10, 45, 48, 259347),
-        #     'modified': datetime.datetime(2024, 5, 19, 11, 25, 4, 422073),
-        #     'created_by': None,
-        #     'updated_by': None,
-        #     'block_type': 'code',
-        #     'metadata': '{"language": "python"}',
-        #     'seq_order': 2,
-        #     'block_visibility': 1,
-        #     'result_visibility': 1,
-        #     'input_schema': '[]',
-        #     'output_schema': '[]',
-        #     'body': 'my_var = "What is 3 plus 2 minus 5"',
-        #     'superbook_id': 21
-        # },
         {
             'id': 5,
             'created': datetime.datetime(2024, 5, 19, 10, 45, 45, 827518),
@@ -56,62 +24,16 @@ json_data = [
             'seq_order': 3,
             'block_visibility': 1,
             'result_visibility': 1,
-            'input_schema': '{"num1": {"type": "number", "description": "The first number.", "default_key": "block-id.sum_result"}, "num2": {"type": "number", "description": "The second number."}}',
-            'output_schema': '{}',
+            'input_schema': {"num1": {"type": "string", "description": "String", "default_key": "block-id.sum_result"}},
+            'output_schema': {
+                'original_string': {'type': 'string', 'description': 'source string'},
+                'translated_string': {'type': 'string', 'description': 'translated result string '}
+            },
             'body': 'res = kwargs["num1"] + kwargs["num2"]\r\nif res >= 0:\r\n    ans = "greater"\r\nelse:\r\n    ans = "less"\r\nprint(ans)\r\nresult = ans',
             'superbook_id': 21,
+            ""
             'location': LLMBlock.default_configuration.location
         },
-        {
-            'id': 3,
-            'created': datetime.datetime(2024, 4, 1, 5, 22, 35, 599490),
-            'modified': datetime.datetime(2024, 5, 19, 11, 13, 47, 749420),
-            'created_by': None,
-            'updated_by': None,
-            'block_type': 'llm_model',
-            'metadata': '{"name": "subtract", "language": "python", "description": "Block to subtract two numbers", "llm_input_var": "my_var"}',
-            'seq_order': 4,
-            'block_visibility': 1,
-            'result_visibility': 1,
-            'input_schema': '{"num1": {"type": "number", "description": "The first number."}, "num2": {"type": "number", "description": "The second number."}}',
-            'output_schema': '{}',
-            'body': 'res = kwargs["num1"] - kwargs["num2"]\r\nif res >= 0:\r\n    ans = "greater"\r\nelse:\r\n    ans = "less"\r\nprint(ans)\r\nprint(res)\r\nresult = ans',
-            'superbook_id': 21,
-            'location': LLMBlock.default_configuration.location
-        },
-        {
-            'id': 2,
-            'created': datetime.datetime(2024, 4, 1, 5, 17, 37, 532541),
-            'modified': datetime.datetime(2024, 5, 19, 11, 19, 47, 938401),
-            'created_by': None,
-            'updated_by': None,
-            'block_type': 'llm_model',
-            'metadata': '{"name": "result", "language": "python", "description": "Result of operations", "llm_block_type": "llm_output", "llm_output_var": "my_llm_output"}',
-            'seq_order': 5,
-            'block_visibility': 1,
-            'result_visibility': 1,
-            'input_schema': '{}',
-            'output_schema': '{"num": {"type": "number", "description": "result of calculations"}, "res": {"type": "string", "description": "summary of result"}}',
-            'body': '',
-            'superbook_id': 21,
-            'location': LLMBlock.default_configuration.location
-        },
-        # {
-        #     'id': 7,
-        #     'created': datetime.datetime(2024, 5, 19, 11, 20, 5, 608776),
-        #     'modified': datetime.datetime(2024, 5, 19, 11, 20, 57, 709155),
-        #     'created_by': None,
-        #     'updated_by': None,
-        #     'block_type': 'code',
-        #     'metadata': '{"language": "python"}',
-        #     'seq_order': 6,
-        #     'block_visibility': 1,
-        #     'result_visibility': 1,
-        #     'input_schema': '{"type": "array", "items": {"type": "string"}}',
-        #     'output_schema': '{"type": "number"}',
-        #     'body': 'print("this is output")\r\nprint(my_llm_output)',
-        #     'superbook_id': 21
-        # }
     ]
 
 
