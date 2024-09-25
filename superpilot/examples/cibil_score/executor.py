c4ed62a7712c223855780050176754f4dc442da6
Shivam <shivamgarg260@gmail.com>
2024-09-25 19:29:09 +0530

    update cibil_score pilot

--- superpilot/examples/cibil_score/executor.py
+++ superpilot/examples/cibil_score/executor.py
@@ -5,13 +5,13 @@ import os
 
 import PyPDF2
 import boto3
+import requests
 from openai import OpenAI
 
 
 
 import pandas as pd
 
-from superpilot.examples.cibil_score.s3 import s3_path_split
 from superpilot.examples.executor.base import BaseExecutor
 
 
@@ -26,31 +26,7 @@ class CibilScoreExecutor(BaseExecutor):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    # Helper function to download the file from an S3 URL
-    def download_from_s3(self, s3_url):
-        # Parse the S3 URL to get bucket and file key
-        bucket_name, file_key = s3_path_split(s3_url)
-        print(os.environ.get('AWS_ACCESS_KEY_ID'), os.environ.get('AWS_SECRET_ACCESS_KEY'))
-
-        # Set up the S3 client and download the file
-        s3 = boto3.client(
-            's3',
-            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
-        )
-
-        s3_object = s3.get_object(Bucket=bucket_name, Key=file_key)
-        file_content = s3_object['Body'].read()  # Binary content of the file
-        return file_content, file_key
-
-
-    def extract_pdf_content_from_s3(self, pdf_binary_content,):
-        """
-        Download a PDF file from S3 and extract text content from it.
-        """
-        # Step 1: Download the file from S3
-
-        # Step 2: Extract text from the PDF content
+    def extract_pdf_content(self, pdf_binary_content,):
         pdf_file = io.BytesIO(pdf_binary_content)  # Convert binary to a file-like object
         reader = PyPDF2.PdfReader(pdf_file)
         content = ""
@@ -61,34 +37,28 @@ class CibilScoreExecutor(BaseExecutor):
 
         return content
 
-    async def execute(self, **kwargs):
+    async def execute(self, files):
         # remove dumy data
-        kwargs = {
-            "content": "Provide the summary of SUNIL Kumar",
-            "files": [
-                {
-                    "object_type": "post",
-                    "object_id": None,
-                    "size": 114143,
-                    "media_id": "873b6364791a11ef9b3266ac95333f55",
-                    "media_type": "pdf",
-                    "media_relation": "attachment",
-                    "name": "KUMAR_M_CIBIL_BC_EVATE.pdf",
-                    "media_url": "https://unpodbackend.s3.amazonaws.com/media/private/KUMAR_M_CIBIL_BC_EVATE.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUVZCV34QRNMNXSNO%2F20240922%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20240922T194032Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=f3f065adc69b7238690bf37741d5e13ab9e2961bd10b122f0cd684f90e1983f3",
-                    "url": "https://unpodbackend.s3.amazonaws.com/media/private/KUMAR_M_CIBIL_BC_EVATE.pdf",
-                    "sequence_number": 0
-                }
-            ],
-            "conversation_type": "initiate",
-            "source": "qa2",
-            "knowledge_bases": [""],
-        }
+        # files = [
+        #             {
+        #                 "object_type": "post",
+        #                 "object_id": None,
+        #                 "size": 114143,
+        #                 "media_id": "873b6364791a11ef9b3266ac95333f55",
+        #                 "media_type": "pdf",
+        #                 "media_relation": "attachment",
+        #                 "name": "KUMAR_M_CIBIL_BC_EVATE.pdf",
+        #                 "media_url": "https://unpodbackend.s3.amazonaws.com/media/private/ASIF_SHARIFF_CIBIL_BC_EVATE_Gd5fw8r.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUVZCV34QRNMNXSNO%2F20240925%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20240925T125407Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=63570e11e5174ca5784be717f4309002ec32e6dae219f487af0000975a31ae38",
+        #                  "url": "https://unpodbackend.s3.amazonaws.com/media/private/KUMAR_M_CIBIL_BC_EVATE.pdf",
+        #                 "sequence_number": 0
+        #             }
+        #         ],
 
-        s3_url = kwargs['files'][0]['media_url']
+        s3_url = files[0]['media_url']
         # Step 1: Download file from S3 URL
-        file_content, file_key = self.download_from_s3(s3_url)
+        file_content = requests.get(s3_url).content
 
-        pdf_text = self.extract_pdf_content_from_s3(file_content)
+        pdf_text = self.extract_pdf_content(file_content)
 
         query = """
         PDF CONTENT: %s
@@ -203,14 +173,18 @@ class CibilScoreExecutor(BaseExecutor):
         print(df_loans, df_consumer)
 
         # Save the DataFrame to Excel
-        # excel_filename = io.BytesIO()
-        excel_filename = "cibil_score.xlsx"
+        excel_filename = io.BytesIO()
+        # excel_filename = "cibil_score.xlsx"
 
         with pd.ExcelWriter(excel_filename, engine='xlsxwriter') as writer:
             df_consumer.to_excel(writer, sheet_name='Consumer Details', index=False)
             df_loans.to_excel(writer, sheet_name='Loans', index=False)
 
-        return f"Data extracted and saved to {excel_filename}"
+        return {
+            'file_content': excel_filename,
+            'type': 'xlsx',
+            'file_name': f"{files[0]['name']}_processed_{files[0]['media_id']}.xlsx"
+        }
 
     async def run(self, query):
         pass
