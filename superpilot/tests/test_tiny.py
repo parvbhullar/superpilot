31800885eeedc22aae2eff2a9905b8c2aa030746
Parvinder <parvinder@recalll.co>
2024-12-20 13:40:49 +0530

    test

---
+++ superpilot/tests/test_tiny.py
@@ -0,0 +1,4 @@
+from tinytroupe.examples import create_lisa_the_data_scientist
+
+lisa = create_lisa_the_data_scientist() # instantiate a Lisa from the example builder
+lisa.listen_and_act("Tell me about your life.")
